04c145ef97930b8b22ed0e72b39e2e6d6595e062
ArtemisCasien <nuk385916@gmail.com>
2026-03-25 18:18:38 +0700

    style(ui) : the style is just born for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import { listen } from "@tauri-apps/api/event";
+import "./styles.css";
 
-function App() {
+export default function App() {
+  const [combo, setCombo] = useState<string>("");
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const unlistenPromise = listen<string>("key_combo", (event) => {
+      const value = event.payload;
+
+      setCombo(value);
+      setVisible(true);
+
+      // auto hide after 1.2s
+      // setTimeout(() => {
+      //   setVisible(false);
+      // }, 1200);
+    });
+
+    return () => {
+      unlistenPromise.then((unlisten) => unlisten());
+    };
+  }, []);
+
+  const keys = combo.split(" + ");
+
   return (
-  <h1> Hello World!</h1>
+    <div className="container">
+      {visible && (
+        <div className="keystroke">
+          {keys.map((key, index) => (
+            <div key={index} className="key">
+              {key}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
-
-export default App;
