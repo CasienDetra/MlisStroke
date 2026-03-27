30edacf9da10f2882840aa598d2d8dead04dbf6f
ArtemisCasien <nuk385916@gmail.com>
2026-03-27 16:22:41 +0700

    feat : system tray added but without icon for now

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,19 +4,37 @@
 mod key_listener;
 
 use std::thread;
-use tauri::Manager;
+use tauri::menu::{MenuBuilder, MenuItemBuilder};
+use tauri::tray::TrayIconBuilder;
 use tauri::Emitter;
+use tauri::Manager;
 
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
-let window = app.get_webview_window("main").unwrap();
+            let window = app.get_webview_window("main").unwrap();
 
+            // Start the key listener in a separate thread
             thread::spawn(move || {
                 key_listener::run_listener(move |combo| {
                     let _ = window.emit("key_combo", combo);
                 });
             });
+
+            // Create the tray menu
+            let quit = MenuItemBuilder::with_id("exit", "Exit").build(app)?;
+            let tray_menu = MenuBuilder::new(app).items(&[&quit]).build()?;
+
+            // Create the tray icon
+            let _ = TrayIconBuilder::new()
+                .menu(&tray_menu)
+                .on_menu_event(|app, event| match event.id.as_ref() {
+                    "exit" => {
+                        app.exit(0);
+                    }
+                    _ => {}
+                })
+                .build(app);
 
             Ok(())
         })
